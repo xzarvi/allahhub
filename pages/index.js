1971b36d7cc00506ef3b5a2ed03f3cce73b56929
xzarvi <88410039+xzarvi@users.noreply.github.com>
2021-09-12 00:11:09 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,15 +16,10 @@ export default function Home() {
           Welcome to <a href="https://www.kebabterminal.tk">Allah X!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="https://kebabterminal.tk/loader.lua" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Click here to get the script.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
@@ -58,9 +53,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          
           </span>
         </a>
       </footer>
